00dad7be74b5e8538ceec62b9b3a90a442bd15b0
Zerik20 <erikaban14@gmail.com>
2024-04-04 16:55:40 -0600

    Cambios de la seccion de las colegiaturas

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,20 @@ export interface FeatureItem {
   icon: string;
   title: string;
 }
+export interface ColItem {
+  price1: string;
+  price2: string;
+  price3: string;
+  cole1: string;
+  cole2: string;
+  extra: string;
+  dp: string;
+  idiom: string;
+  horario: string;
+  icon: string;
+  title: string;
+  title2: string;
+}
 
 export interface FooterLink {
   description: string;
